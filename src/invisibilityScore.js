e3237b298a0368fd588c69b8fc53a437f33e53b5
Emma <emmabaldachin@gmail.com>
2023-09-11 18:27:14 +0100

    basic post writing file to S3 working locally

--- src/invisibilityScore.js
+++ src/invisibilityScore.js
@@ -1,11 +1,23 @@
 
-
 const invisibilityScore = async (event) => {
+  console.log(event)
+  const {uuid, invisibilityScore, status} = event;
+  const AWS = require('aws-sdk');
+  const { parse } = require('json2csv')
+  const s3 = new AWS.S3();
+  const data = {
+    uuid,
+    invisibilityScore,
+    status
+  }
+  const csv = parse(data);
+  const res = await s3.putObject({ Bucket: "invisibility-score-bucket", Key: data.uuid + '.csv', Body: csv }).promise();
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Invisibility Score",
+        message: 'THIS IS THE RESPONSE: ' + res,
         input: event,
       },
       null,
@@ -15,5 +27,5 @@ const invisibilityScore = async (event) => {
 };
 
 module.exports = {
-  handler: invisibilityScore,
+  handler : invisibilityScore
 }
